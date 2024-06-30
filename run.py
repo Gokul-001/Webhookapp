306af56bfa7006b0c8b92c96ee044fa3909a273a
Gokul-001 <gokul3krish2@gmail.com>
2024-06-30 14:46:52 +0530

    env changes

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ from dotenv import load_dotenv
 from app import create_app
 from app.logcontrol import logger
 
+load_dotenv()
 app = create_app()
-logger("app").info("App created")
-
+host = os.getenv('host', '127.0.0.1')
+port = os.getenv('port', 8000)
 
 if __name__ == '__main__': 
-    load_dotenv()
     logger("app").info("App running...")
     from waitress import serve
-    serve(app, host=os.getenv("host"), port=os.getenv('port'))
-    #app.run(debug=True,port=os.getenv('port'))
+    serve(app, host=host, port=int(port))
+    
     
